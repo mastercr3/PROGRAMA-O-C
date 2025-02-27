c177d29fadbb11ebe4c671237cd324119153da8a
Jordan Silva <diogomastercr@gmail.com>
2025-02-27 22:54:02 +0000

    conclusão nivel basico supertrunfo

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,35 +4,28 @@
 
 int main (){
 
-char estado1;
-char card1 [20];
-char cid1 [20];
-int popu1;
-float area1;
-float pib1;
-int turis1;
-
-char estado2;
-char card2 [20];
-char cid2 [20];
-int popu2;
-float area2;
-float pib2;
-int turis2;
+char estado1, estado2; // variaveis para armazenar a letra do estado
+char card1 [20], card2 [20]; // variaveis para armazenar o codigo da carta   
+char cid1 [20], cid2 [20]; // variaveis para armazenar o nome da cidade
+int popu1, popu2; // variaveis para armazenar a populacao
+float area1, area2; // variaveis para armazenar a area em km²
+float pib1, pib2; // variaveis para armazenar o PIB
+int turis1, turis2; // variaveis para armazenar o numero de pontos turisticos
 
 
+    printf("Caro usuario, cadastre os dados referente a carta 1: \n\n");
 
-    printf("CARTA 1: \n\n");
-
-    printf("atribua uma letra ao estado:\n");
+    printf("Atribua uma letra ao estado:\n");
     scanf("%s", &estado1);
 
     printf("Atribua um codigo para a carta:\n");
     scanf("%s", card1);
+    
 
     printf("Informe o nome da cidade:\n");
-    scanf("%s", cid1);
-
+    scanf("%s", cid1); 
+    
+    
     printf("Informe a populacao:\n");
     scanf("%d", &popu1);
 
@@ -46,17 +39,17 @@ int turis2;
     scanf("%d", &turis1);
 
 
-printf ("CARTA 2: \n\n");
+    printf ("Agora vamos cadastrar os dados para a carta 2: \n\n");
 
-    printf("atribua uma letra ao estado:\n");
+    printf("Atribua uma letra ao estado:\n");
     scanf("%s", &estado2);
 
     printf("Atribua um codigo para a carta:\n");
     scanf("%s", card2);
 
     printf("Informe o nome da cidade:\n");
-    scanf("%s", cid2);
-
+    scanf("%s", cid2); 
+   
     printf("Informe a populacao:\n");
     scanf("%d", &popu2);
 
@@ -72,16 +65,16 @@ printf ("CARTA 2: \n\n");
 
 
 
-printf ("CARTA 1: \n\n");
+printf ("CARTA 1: \n");
 printf ("Estado: %c\n", estado1);  
 printf ("Codigo: %s\n", card1);
 printf ("Cidade: %s\n", cid1);
 printf ("Populacao: %d\n", popu1);
 printf ("Total da area: %.2f km²\n", area1);
 printf ("PIB: %.2f bilhoes de reais.\n", pib1);
-printf ("Numero de pontos turistiscos: %d\n", turis1);
+printf ("Numero de pontos turistiscos: %d\n\n", turis1);
 
-printf ("CARTA 2: \n\n");
+printf ("CARTA 2: \n");
 printf ("Estado: %c\n", estado2);
 printf ("Codigo: %s\n", card2);
 printf ("Cidade: %s\n", cid2);
@@ -91,8 +84,6 @@ printf ("PIB: %.2f bilhoes de reais.\n", pib2);
 printf ("Numero de pontos turistiscos: %d\n", turis2);
 
     
-
-
 
 return 0;
 
